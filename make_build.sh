423e7c51bf8c2f55ae13ab1de57a2b24f30b5ef5
Ben Soille <benjamin.soille@easiware.fr>
2021-05-18 01:19:57 +0200

    Moved badly located requirements file

--- make_build.sh
+++ make_build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # Minikube special case : use minikube local docker registry
+echo "----- Setting up Minikube local Docker registry -----"
 eval $(minikube docker-env)
 docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
 cd medicine_pubsub/
 
+echo "----- Building _Patient_ docker image -----"
 docker -D build -f patient/Dockerfile -t localhost:5000/ben_loftorbital/test_patient_0 .
+
+echo "----- Building _Medicine_ docker image -----"
 
 cd -
--- make_deploy.sh
+++ make_deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+echo "----- Deploying patient to kubernetes -----"
 kubectl apply -f deploy/patient.yaml -n default
+
